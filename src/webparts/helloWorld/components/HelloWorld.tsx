43d416ba781e58aacc732eb71950f70f4114e7fa
Naufal Khaddafy <naufalkhaddafy136@gmail.com>
2025-06-15 19:24:45 +0800

    add: bootstrap 5

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -1,7 +1,9 @@
-import * as React from 'react';
-import styles from './HelloWorld.module.scss';
-import type { IHelloWorldProps } from './IHelloWorldProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import styles from "./HelloWorld.module.scss";
+import type { IHelloWorldProps } from "./IHelloWorldProps";
+import { escape } from "@microsoft/sp-lodash-subset";
 
 export default class HelloWorld extends React.Component<IHelloWorldProps> {
   public render(): React.ReactElement<IHelloWorldProps> {
@@ -10,32 +12,98 @@ export default class HelloWorld extends React.Component<IHelloWorldProps> {
       isDarkTheme,
       environmentMessage,
       hasTeamsContext,
-      userDisplayName
+      userDisplayName,
     } = this.props;
 
     return (
-      <section className={`${styles.helloWorld} ${hasTeamsContext ? styles.teams : ''}`}>
+      <section
+        className={`${styles.helloWorld} ${
+          hasTeamsContext ? styles.teams : ""
+        }`}
+      >
         <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
+          <img
+            alt=""
+            src={
+              isDarkTheme
+                ? require("../assets/welcome-dark.png")
+                : require("../assets/welcome-light.png")
+            }
+            className={styles.welcomeImage}
+          />
           <h2>Well done, {escape(userDisplayName)}!</h2>
           <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
+          <div>
+            Web part property value: <strong>{escape(description)}</strong>
+          </div>
         </div>
         <div>
           <h3>Welcome to SharePoint Framework!</h3>
           <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
+            The SharePoint Framework (SPFx) is a extensibility model for
+            Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest
+            way to extend Microsoft 365 with automatic Single Sign On, automatic
+            hosting and industry standard tooling.
           </p>
           <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
+          <div className="container mt-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Halo dari Bootstrap 5!</h5>
+                <p className="card-text">
+                  Ini contoh WebPart SPFx pakai Bootstrap.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Klik Saya
+                </a>
+
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
+                  Launch demo modal
+                </button>
+
+                <div
+                  className="modal fade"
+                  id="exampleModal"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">
+                          Modal title
+                        </h5>
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div className="modal-body">...</div>
+                      <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button type="button" className="btn btn-primary">
+                          Save changes
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     );
